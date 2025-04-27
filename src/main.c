91b46c5f3b3cb9a3d96fbcc52d7413c23de29e9e
Almir Neto <almirneto338@gmail.com>
2025-04-27 00:21:20 -0300

    feet: adding function to get platform informations refactor: moving get number of platforms to function

--- src/main.c
+++ src/main.c
@@ -7,10 +7,28 @@
 
 #include <CL/cl.h>
 
-int main(int argc, char const *argv[]) {
+cl_uint getNumberOfPlatforms() {
     cl_int numberOfPlatforms = 0;
     clGetPlatformIDs(0, NULL, &numberOfPlatforms);
+    return numberOfPlatforms;
+}
 
+void listAvailablePlatforms(cl_platform_id platforms[],
+                            cl_uint numberOfPlatforms) {
+    char name[255], version[255], profile[255], vendor[255];
+
+    for (int i = 0; i < numberOfPlatforms; i++) {
+        clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, -1, name, NULL);
+        clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, -1, version, NULL);
+        clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, -1, profile, NULL);
+        clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, -1, vendor, NULL);
+        printf("-> %s\n\t-> %s\n\t->%s\n\t->%s\n", name, version, profile,
+               vendor);
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    cl_uint numberOfPlatforms = getNumberOfPlatforms();
     if (numberOfPlatforms > 0) {
         printf("-> Number of available OpenCL platforms: %d\n",
                numberOfPlatforms);
@@ -18,6 +36,12 @@ int main(int argc, char const *argv[]) {
         printf("-> No OpenCL platform found!\n");
         return 1;
     }
+
+    cl_platform_id platforms[numberOfPlatforms];
+
+    clGetPlatformIDs(numberOfPlatforms, platforms, NULL);
+
+    listAvailablePlatforms(platforms, numberOfPlatforms);
 
     return 0;
 }
